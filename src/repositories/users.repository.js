53ce89a251682fca5ebca70e96f98e7ce04fb6ff
soobeen0301 <xg12041@gmail.com>
2024-06-13 17:11:09 +0900

    Feat : 사용자 정보 조회 API layer분리

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,1 +1,19 @@
-export class UsersRepository {}
+import { prisma } from '../utils/prisma.util.js';
+
+export class UsersRepository {
+  async findUserId(id) {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return user;
+  }
+}
